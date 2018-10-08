31969264c78cc576fc8e35faa375a29c73d539e8
unknown <paulo@enabe.com.br>
2018-10-08 17:08:59 -0300

    Akira pegando

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,11 @@
 //= require turbolinks
 //= require materialize-sprockets
 //= require_tree .
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'left',
+      hoverEnabled: false
+    });
+  });
