6905168eb684945280055367adb96555a02cfb7e
André Reis <andre.souza.reis@usp.br>
2018-11-05 14:59:04 -0200

    Andre 2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,9 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
 
-  document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems);
   });
-
-  $(".dropdown-content>li>a").css("color", themeColor);
