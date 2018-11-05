22bd1e7f3d6b12978af06fae8e800c28bcc9f3fa
unknown <paulo@enabe.com.br>
2018-11-05 15:48:48 -0200

    Update- paginas materias

--- app/controllers/materia_controller.rb
+++ app/controllers/materia_controller.rb
@@ -1,4 +1,31 @@
 class MateriaController < ApplicationController
   def matematica
   end
+
+  def fisica
+  end
+
+  def biologia
+  end
+
+  def geografia
+  end
+
+  def historia
+  end
+
+  def ingles
+  end
+
+  def quimica
+  end
+
+  def sociologia
+  end
+
+  def filosofia
+  end
+
+  def linguas
+  end
 end
